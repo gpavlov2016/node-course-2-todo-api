14ecc62895cdc24415f0b060900da46cab9b6912
Guy Pavlov <guy.pavlov84@gmail.com>
2018-10-26 14:08:24 -0400

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.post('/todos', (req, res) => {
   todo.save().then((doc) => {
     res.send(doc);
   }, (e) => {
-    res.status(400).send(e);
+    res.status(400).send();
   });
 });
 
@@ -27,7 +27,7 @@ app.get('/todos', (req, res) => {
   Todo.find({}).then((todos) => {
     res.send({todos});
   }, (e) => {
-    res.status(400).send(e);
+    res.status(400).send();
   });
 });
 
@@ -45,7 +45,25 @@ app.get('/todos/:id', (req, res) => {
     }
     res.send({todo});
   }, (e) => {
-    res.status(400).send(e);
+    res.status(400).send();
+  });
+});
+
+// DELETE /todos/1234
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send('Invalid ID')
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send('ID not found')
+    }
+    res.send({todo});
+  }, (e) => {
+    res.status(400).send();
   });
 });
 
