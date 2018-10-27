c381e1e33dd237253ec839ed6dedd0090c9837c4
Guy Pavlov <guy.pavlov84@gmail.com>
2018-10-27 00:53:50 -0400

    Add tests for GET /users/me and POST /users

--- server/server.js
+++ server/server.js
@@ -106,11 +106,9 @@ app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
 
-  console.log(user);
   user.save().then(()  => {
     return user.generateAuthToken();
   }).then((token) => {
-    console.log(token);
     res.header('x-auth', token).send(user);
   }).catch((e) => {
     res.status(400).send(e);
