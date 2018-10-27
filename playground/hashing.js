11167c58cfb989209ff9259d81060ccb2043e606
Guy Pavlov <guy.pavlov84@gmail.com>
2018-10-26 23:29:47 -0400

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,41 +1,56 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
+var password = '123abc';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+var hashedPassword = '$2a$10$2pqa2MoTIp9MgbQ5G1blke4qOP/dpHWOo62ARp2azLaX4sJk9NtiW';
 
-
-//
-// var message = 'I am user number 3';
-//
-// var hash = SHA256(message).toString();
-//
-// console.log(`Message: ${message}`);
-// console.log(`Hash: ${hash}`);
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
 //
 // var data = {
-//   id: 4
+//   id: 10
 // };
 //
-// var token = {
-//   data: data,
-//   hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
-// }
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
 //
-// // token.data.id = 5;
-// // token.hash = SHA256(JSON.stringify(token.data)).toString();
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
 //
-// var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
 //
-// if (resultHash === token.hash) {
-//   console.log('Data was not changed');
-// } else {
-//   console.log('Data was changed. Do not trust!');
-// }
+// //
+// // var message = 'I am user number 3';
+// //
+// // var hash = SHA256(message).toString();
+// //
+// // console.log(`Message: ${message}`);
+// // console.log(`Hash: ${hash}`);
+// //
+// // var data = {
+// //   id: 4
+// // };
+// //
+// // var token = {
+// //   data: data,
+// //   hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
+// // }
+// //
+// // // token.data.id = 5;
+// // // token.hash = SHA256(JSON.stringify(token.data)).toString();
+// //
+// // var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
+// //
+// // if (resultHash === token.hash) {
+// //   console.log('Data was not changed');
+// // } else {
+// //   console.log('Data was changed. Do not trust!');
+// // }
--- server/server.js
+++ server/server.js
@@ -106,9 +106,11 @@ app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
 
+  console.log(user);
   user.save().then(()  => {
     return user.generateAuthToken();
   }).then((token) => {
+    console.log(token);
     res.header('x-auth', token).send(user);
   }).catch((e) => {
     res.status(400).send(e);
