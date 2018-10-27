e3b017da7b72e9853e5aeeba798901a8243169f3
Guy Pavlov <guy.pavlov84@gmail.com>
2018-10-27 13:18:18 -0400

    Add authentication to /todos routes

--- server/server.js
+++ server/server.js
@@ -15,23 +15,26 @@ var port = process.env.PORT;
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req, res) => {
+app.post('/todos', authenticate, (req, res) => {
   var todo = new Todo({
-    text: req.body.text
+    text: req.body.text,
+    _creator: req.user._id
   });
 
   todo.save().then((doc) => {
     res.send(doc);
   }, (e) => {
-    res.status(400).send();
+    res.status(400).send(e);
   });
 });
 
-app.get('/todos', (req, res) => {
-  Todo.find({}).then((todos) => {
+app.get('/todos', authenticate, (req, res) => {
+  Todo.find({
+    _creator: req.user._id
+  }).then((todos) => {
     res.send({todos});
   }, (e) => {
-    res.status(400).send();
+    res.status(400).send(e);
   });
 });
 
